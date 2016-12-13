accfc0c8042fd6abae1b6d151e9fe8053e2c750c
Shawn Hicks <shicks@e1z2r5p3.42.us.org>
2016-12-12 21:36:53 -0800

    Adding the working readfile portion of program.

--- ft_readfile.c
+++ ft_readfile.c
@@ -10,71 +10,119 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libfil.h"
+#include "./libft/libft.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-//char		*piece_compare(char *buff)
-//{
-//
-//}
-
-char		*ft_piecetrim(char *s)
+void		piece_compare(char **bufftrim, char **stor, int istor)
 {
-	char	*new;
-	int		start;
-	int		end;
-	int		i;
-
-	i = 0;
-	end = 0;
-	start = 0;
-	if (!s)
-		return (NULL);
-	while (s[start] != '#')
-		start++;
-	while (s[end] != '\0')
-	{
-		if (s[end] == '\n')
-			s[end] = '.';
-		end++;
-	}
-	while (s[end] != '#' && end > start)
-		end--;
-	if (!(new = ft_strnew(end - start)))
-		return (NULL);
-	while (start <= end)
-	{
-		if (s[start] == '\n')
-			++start;
-		if (s[start] != '\n')
-			new[i] = s[start];
-			i++;
-			start++;
-	}
-	new[i] = '\0';
-	return (new);
+	if (ft_strequ((*bufftrim), "##...##") == 1)
+		(*stor)[istor] = 'A';
+	else if (ft_strequ((*bufftrim), "####") == 1)
+		(*stor)[istor] = 'B';
+	else if (ft_strequ((*bufftrim), "#....#....#....#") == 1)
+		(*stor)[istor] = 'C';
+	else if (ft_strequ((*bufftrim), "#...##...#") == 1)
+		(*stor)[istor] = 'D';
+	else if (ft_strequ((*bufftrim), "##....##") == 1)
+		(*stor)[istor] = 'E';
+	else if (ft_strequ((*bufftrim), "##..##") == 1)
+		(*stor)[istor] = 'F';
+	else if (ft_strequ((*bufftrim), "#....##....#") == 1)
+		(*stor)[istor] = 'G';
+	else if (ft_strequ((*bufftrim), "#....###") == 1)
+		(*stor)[istor] = 'H';
+	else if (ft_strequ((*bufftrim), "#....#...##") == 1)
+		(*stor)[istor] = 'I';
+	else if (ft_strequ((*bufftrim), "###....#") == 1)
+		(*stor)[istor] = 'J';
+	else if (ft_strequ((*bufftrim), "##...#....#") == 1)
+		(*stor)[istor] = 'K';
+	else if (ft_strequ((*bufftrim), "#..###") == 1)
+		(*stor)[istor] = 'L';
+	else if (ft_strequ((*bufftrim), "#....#....##") == 1)
+		(*stor)[istor] = 'M';
+	else if (ft_strequ((*bufftrim), "###..#") == 1)
+		(*stor)[istor] = 'N';
+	else if (ft_strequ((*bufftrim), "##....#....#") == 1)
+		(*stor)[istor] = 'O';
+	else if (ft_strequ((*bufftrim), "#...###") == 1)
+		(*stor)[istor] = 'P';
+	else if (ft_strequ((*bufftrim), "#....##...#") == 1)
+		(*stor)[istor] = 'Q';
+	else if (ft_strequ((*bufftrim), "###...#") == 1)
+		(*stor)[istor] = 'R';
+	else if (ft_strequ((*bufftrim), "#...##....#") == 1)
+		(*stor)[istor] = 'S';
 }
 
-void		ft_checkpiece(int argc, char **argv)
+char		*ft_piecetrim(char **bufftrim, char **buff, char **stor)
+{
+	int		i;
+	int		start;
+	int		hash;
+
+	i = 0;
+	hash = 0;
+	start = 0;
+	while ((*buff)[start] != '\0')
+	{
+		if ((*buff)[start] == '\n')
+			(*buff)[start] = '.';
+		start++;
+	}
+	start = 0;
+	while (hash < 4 && (*buff)[start] != '\0')
+	{
+		if ((*buff)[start] == '#')
+			hash++;
+		if (hash >= 1)
+		{
+			(*bufftrim)[i] = (*buff)[start];
+			i++;
+		}
+		start++;
+	}
+	(*bufftrim)[i] = '\0';
+	return (*bufftrim);
+}
+
+char		*ft_readpieces(int argc, char **argv)
 {
 	int		fd_input;
 	int 	read_length;
+	int		istor;
 	char	*buff;
+	char	*bufftrim;
+	char	*stor;
 
+	istor = 0;
 	fd_input = open(argv[1], O_RDONLY);
-	buff = (char*)malloc(sizeof(char) * 21);
+	buff = ft_strnew(21);
+	bufftrim = ft_strnew(21);
+	stor = ft_strnew(10000);
 	read_length = 21;
 	while (read_length == 21)
 	{
 		read_length = read(fd_input, buff, 21);
 		buff[read_length] = '\0';
-		printf("%s\n", buff);
-		ft_piecetrim(buff);
-		printf("%s\n\n", buff);
+		bufftrim = ft_piecetrim(&bufftrim, &buff, &stor);
+		piece_compare(&bufftrim, &stor, istor);
+		istor++;
 	}
+	return (stor);
 }
 
-int			main(int argc, char **argv)
-{
-	ft_checkpiece(argc, argv);
-	return (0);
-}
+//int			main(int argc, char **argv)
+//{
+//	char	*pieces;
+//
+//	istor = 0;
+//	pieces = ft_readpieces(argc, argv);
+//	ft_putstr(pieces);
+//	return (0);
+//}
